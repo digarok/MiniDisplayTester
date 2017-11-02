79665c39e00c22cfa70463be5c2631967886cda6
Dagen Brock <dagenbrock@gmail.com>
2017-11-01 22:48:50 -0500

    adding support files for building

---
+++ env.sh
@@ -0,0 +1,10 @@
+# DEVELOPMENT ENVIRONMENT VARIABLES
+#
+# Set these to where you have the matching
+# utilities installed on your system
+
+
+MERLIN32="/Users/dbrock/appleiigs/BrutalDeluxeCE/Merlin32_v1.0/Source/Merlin32"
+CADIUS="/Users/dbrock/appleiigs/BrutalDeluxeCE/Cadius_v1.1/Src/cadius"
+GSEMU="/Users/dbrock/appleiigs/gsplus/src/gsplus"
+
