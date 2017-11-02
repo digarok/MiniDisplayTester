e5c0c17a6a86ea37ee3dfc58132f760c01891eb8
Dagen Brock <dagenbrock@gmail.com>
2017-11-02 08:57:07 -0500

    cleanups

--- env.sh
+++ env.sh
@@ -7,4 +7,11 @@
 MERLIN32="/Users/dbrock/appleiigs/BrutalDeluxeCE/Merlin32_v1.0/Source/Merlin32"
 CADIUS="/Users/dbrock/appleiigs/BrutalDeluxeCE/Cadius_v1.1/Src/cadius"
 GSEMU="/Users/dbrock/appleiigs/gsplus/src/gsplus"
+RADIUS="/Users/dbrock/appleiigs/radius/radius.rb"
+RUBY="ruby"
 
+# Use these settings for auto-indentation
+# "radius" : indent with radius
+# "cadius" : indent with cadius
+# "none"   : no auto-indentation
+INDENTATION="radius"
